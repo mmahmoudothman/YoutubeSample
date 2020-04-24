69f90f64a3947a30a533693c412262c7a94e550b
osos <mahmoudosman@paymobsolutions.com>
2020-04-24 04:32:03 +0200

    Init Project

---
+++ app/src/main/java/com/example/exomedia/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.exomedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.devbrackets.android.exomedia.listener.OnPreparedListener;
+import com.devbrackets.android.exomedia.ui.widget.VideoView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youtube_play_view;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        youtube_play_view = findViewById(R.id.youtube_play_view);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("a4NT5iBFuZs");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youtube_play_view.initialize("AIzaSyCIX-xbGaRou0t_eG3gnrNyXCF7iFEBmgo", onInitializedListener);
+    }
+}
